5179891562b1af758e5932b2b66e707b60fbbc33
Zil Pavdighada <zil.pavdigh@gmail.com>
2025-06-26 22:35:58 -0400

    done for the day

--- tryingRequests.py
+++ tryingRequests.py
@@ -3,19 +3,26 @@ import time
 
 URLS = ["https://api.github.com", "https://api.github.com/invalid"]
 
-start_time = time.perf_counter()
 for url in URLS:
     try:
-        
         response = requests.get(url)
         response.raise_for_status()
     except:
         print(f"Error code: {response.status_code}")
     else:
         print("Success!")
+
+
+
+
+start_time = time.perf_counter()
+r = requests.get("https://api.github.com")
+
+print(r.text)
+
+print(type(r.text))
+
+
 end_time = time.perf_counter()
 
 print(f"Time taken: {end_time - start_time:.2f} seconds")
-
-
-
