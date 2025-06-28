a4510bfe28ce82eca40cdd1412cfd3acd4d4a0bd
Zil Pavdighada <zil.pavdigh@gmail.com>
2025-06-27 22:48:36 -0400

    a lil more

--- tryingRequests.py
+++ tryingRequests.py
@@ -1,27 +1,31 @@
 import requests
 import time
 
-URLS = ["https://api.github.com", "https://api.github.com/invalid"]
+# URLS = ["https://api.github.com", "https://api.github.com/invalid"]
 
-for url in URLS:
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except:
-        print(f"Error code: {response.status_code}")
-    else:
-        print("Success!")
-
-
+# for url in URLS:
+#     try:
+#         response = requests.get(url)
+#         response.raise_for_status()
+#     except:
+#         print(f"Error code: {response.status_code}")
+#     else:
+#         print("Success!")
 
 
-start_time = time.perf_counter()
+
+
+
 r = requests.get("https://api.github.com")
 
-print(r.text)
+# print(r.text)
 
-print(type(r.text))
+# print(type(r.text))
 
+start_time = time.perf_counter()
+
+print(r.headers["content-type"])
+print(type(r.headers))
 
 end_time = time.perf_counter()
 
